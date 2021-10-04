f0f7d7bf1bfbe8667b71ceff1c401ab356db3125
CD4ny <chavianodaniel99@gmail.com>
2021-10-04 07:24:19 -0400

    Scratch project Added Redux Added Material

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Button  from '@material-ui/core/Button' ;
+import Toolbar from '@material-ui/core/Toolbar';
+import React, {Component} from 'react'	   ;
+
+class App extends Component {
+
+	constructor(args: any) {
+		super(args);
+
+	}
+
+	render() {
+		return (
+			<div>
+
+			</div>
+		);
+	}
+}
+
+export default App;
