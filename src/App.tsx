14e9f6503bedc3875946634dbad505737354c7fa
CD4ny <chavianodaniel99@gmail.com>
2021-10-06 18:15:16 -0400

    First Build

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-// import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography";
-import MenuIcon from "@material-ui/icons/Menu"
-import SearchIcon from "@material-ui/icons/GitHub"
-import ContactMailRounded from "@material-ui/icons/ContactMailRounded"
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/GitHub";
+import ContactMailRounded from "@material-ui/icons/ContactMailRounded";
 import { makeStyles, IconButton } from "@material-ui/core";
 
 const useStyle = makeStyles((theme) => ({
