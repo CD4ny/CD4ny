2ac5e6ce672a0c9e6172684dbd80b4ee1befc787
CD4ny <chavianodaniel99@gmail.com>
2021-10-14 02:41:24 -0400

    Commit loco

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,66 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
-import { makeStyles, IconButton } from "@material-ui/core";
+import { makeStyles, IconButton, Grid } from "@material-ui/core";
 import PersonalCard from "./PersonalCard";
+import UsersList from "./users.json"
 
 const useStyle = makeStyles((theme) => ({
-	offset:theme.mixins.toolbar
+	offset: theme.mixins.toolbar
 	,
-	menuIcon:{
-		marginRight:theme.spacing(2)
+	menuIcon: {
+		marginRight: theme.spacing(2)
 	},
-	Title:{
-		flexGrow:1,
+	Title: {
+		flexGrow: 1,
 	}
 }));
+
+interface User {
+	name: string
+	age: string
+}
+
+function getUserInfo<T>(nick: String): Promise<T> {
+	return fetch('https://api.github.com/users/' + nick)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error(response.statusText)
+			}
+			return response.json().then(data => data as T);
+		})
+
+
+}
+
+async function CardsList(){
+	const list: any[] = [0]
+	for (let i = 0; i < UsersList.length; i++) {
+		const info = await getUserInfo<{}>(UsersList[i]);
+		let asd = JSON.stringify(info)
+		const avatar_url = JSON.parse(asd)["avatar_url"]
+		const name = JSON.parse(asd)["name"]
+		const bio = JSON.parse(asd)["bio"]
+
+		const github = JSON.parse(asd)["html_url"]
+		const twitter = JSON.parse(asd)["twitter_username"]
+		const page = JSON.parse(asd)["blog"]
+
+		const persona = {
+			"nick": name,
+			"bio": bio,
+			"avatar": avatar_url,
+			"Twitter": twitter,
+			"GitHub": github,
+			"Page": page
+		};
+		list[i] = persona
+
+	}
+
+	return await list;
+}
+ const list = CardsList();
 
 const Navbar = () => {
 
@@ -25,16 +72,24 @@ const Navbar = () => {
 		<React.Fragment>
 			<AppBar color="primary"  >
 				<Toolbar>
-					<Typography variant="h5" className={classes.Title}>CD4ny!</Typography>
-					<IconButton color="inherit" className={classes.menuIcon} aria-label="menu" onClick={() => { } }>
+					<Typography variant="h5" className={classes.Title} style={{ wordWrap: "break-word" }}>CD4ny!</Typography>
+					<IconButton color="inherit" className={classes.menuIcon} aria-label="menu" onClick={() => {
+
+					}}>
 						<MenuIcon>
-						</MenuIcon>						
+						</MenuIcon>
 					</IconButton>
 				</Toolbar>
 			</AppBar>
 			<div className={classes.offset}></div>
-			
-			<PersonalCard nick="DAniel Chaviano" bio="Unnecesart explanations guide to hell" avatar="https://avatars.githubusercontent.com/u/64395192?v=4" />
+			<Grid container spacing={0} alignContent="flex-start" justifyContent="flex-start">
+				{list.map(
+					(d:any, i: Number) => {
+						return <PersonalCard nick={d.nick} bio={d.bio}></PersonalCard>
+					}
+				)
+				}
+			</Grid>
 		</React.Fragment>
 	);
 };
--- src/PersonalCard.tsx
+++ src/PersonalCard.tsx
@@ -7,7 +7,6 @@ import FacouriteIcon from "@material-ui/icons/FavoriteRounded"
 import Twitter from "@material-ui/icons/Twitter"
 import GitHub from "@material-ui/icons/GitHub"
 import Email from "@material-ui/icons/Email"
-
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
@@ -15,6 +14,8 @@ import Avatar from "@material-ui/core/Avatar";
 import { createTheme } from "@material-ui/core/styles";
 import CardActions from "@material-ui/core/CardActions";
 import IconButton from "@material-ui/core/IconButton";
+import FindInPage from "@material-ui/icons/FindInPage";
+import { Language } from "@material-ui/icons";
 
 
 
@@ -23,11 +24,15 @@ const PersonalCard = (props: any) => {
         breakpoints: {
             values: {
                 xs: 0,
+                sm: 600,
+                md: 900,
+                lg: 1200,
+                xl: 1536,
+                // sm: 596,
+                // md: 992,
+                // lg: 1196,
+                // xl: 1020,
 
-                sm: 596,
-                md: 992,
-                lg: 1196,
-                xl: 1020,
             },
         },
     });
@@ -41,45 +46,54 @@ const PersonalCard = (props: any) => {
     }));
     return (
         <ThemeProvider theme={theme}>
-            <Grid xs={12} sm={6} md={3} lg={3} xl={4}>
-                <Card elevation={3}>
-                    <Grid container justifyContent="flex-start"
-                        alignItems="center" spacing={0} >
+            <Grid xs={12} sm={12} md={6} lg={4} xl={3}>
+                <Box mx={1} my={1} >
+                    <Paper elevation={3}>
+                        <Card variant="outlined" >
+                            <Grid container justifyContent="flex-start"
+                                alignItems="center" spacing={0} >
 
-                        <Box m={1}>
+                                <Box m={1}>
 
-                            <Avatar style={{ height: '75px', width: '75px' }} alt={props.nick.toString()} src={props.avatar.toString()} />
+                                    <Avatar style={{ height: '75px', width: '75px' }} alt={props.nick} src={props.avatar} />
 
-                        </Box>
-                        <Box m={1} style={{ flexGrow: 1 }}>
-                            <Typography variant="h5" color="initial">
-                                {props.nick}
-                            </Typography>
-                            <Typography variant="subtitle1" color="initial">
-                                {props.bio}
-                            </Typography>
-                            <Grid container justifyContent="flex-end"
-                                alignItems="center" spacing={0}>
-                                <IconButton aria-label="add to favorites"
-                                    onClick={() => { window.location.assign("http://127.0.0.1") }}
-                                >
-                                    <Twitter />
-                                </IconButton>
-                                <IconButton aria-label="share"
-                                    onClick={() => { }}>
-                                    <GitHub />
-                                </IconButton>
-                                <IconButton aria-label="share"
-                                    onClick={() => { }}>
-                                    <Email />
-                                </IconButton>
+                                </Box>
+                                <Box m={1} style={{ flexGrow: 1 }}>
+                                    <Typography variant="h5" color="initial" style={{ wordWrap: "break-word" }}>
+                                        {props.nick}
+                                    </Typography>
+                                    <Typography variant="subtitle1" color="initial">
+                                        {props.bio}
+                                    </Typography>
+
+                                    <Grid container justifyContent="flex-end"
+                                        alignItems="center" spacing={0}>
+                                        <IconButton aria-label="twitter" disabled={props.Twitter === null || props.Twitter === undefined}
+                                            onClick={() => { window.location.assign(props.Twitter) }}
+                                            style={{ color: (props.Twitter !== null && props.Twitter !== undefined ? "#1d9bf0" : "#6c899d"), }}
+                                        >
+                                            <Twitter />
+                                        </IconButton>
+                                        <IconButton aria-label="GitHub"
+                                            disabled={props.GitHub === null || props.GitHub === undefined}
+                                            onClick={() => { window.location.assign(props.GitHub) }}
+                                            style={{ color: (props.GitHub !== null && props.GitHub !== undefined ? "#000" : "#888"), }}>
+                                            <GitHub />
+                                        </IconButton>
+                                        <IconButton aria-label="Page" color="primary"
+                                            disabled={props.Page === null || props.Page === undefined}
+                                            onClick={() => { window.location.assign(props.Page) }}>
+                                            <Language />
+                                        </IconButton>
+                                    </Grid>
+                                </Box>
+
                             </Grid>
-                        </Box>
-
-                    </Grid>
 
 
-                </Card>
+                        </Card>
+                    </Paper>
+                </Box>
             </Grid>
         </ThemeProvider>
     )
