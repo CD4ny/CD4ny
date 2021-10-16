dc199e15817afedc5ea1375751a748a7a48ff79f
CD4ny <chavianodaniel99@gmail.com>
2021-10-15 18:04:45 -0700

    Axios installed changed Changed main frame paradigm to Component instead of function

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,70 @@
-import React from "react";
+import { Grid, IconButton } from "@material-ui/core";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
-import { makeStyles, IconButton, Grid } from "@material-ui/core";
+import { withStyles } from '@material-ui/styles';
+import React, { Component } from "react";
 import PersonalCard from "./PersonalCard";
-import UsersList from "./users.json"
 
-const useStyle = makeStyles((theme) => ({
-	offset: theme.mixins.toolbar
+
+const useStyle = (theme: any) => ({
+	offset: {
+		minHeight: 56
+	}
 	,
-	menuIcon: {
-		marginRight: theme.spacing(2)
-	},
 	Title: {
 		flexGrow: 1,
 	}
-}));
+})
 
-interface User {
-	name: string
-	age: string
+interface IProps {
+	classes: any
 }
 
-function getUserInfo<T>(nick: String): Promise<T> {
-	return fetch('https://api.github.com/users/' + nick)
-		.then(response => {
-			if (!response.ok) {
-				throw new Error(response.statusText)
-			}
-			return response.json().then(data => data as T);
-		})
-
-
+interface IState {
 }
 
-async function CardsList(){
-	const list: any[] = [0]
-	for (let i = 0; i < UsersList.length; i++) {
-		const info = await getUserInfo<{}>(UsersList[i]);
-		let asd = JSON.stringify(info)
-		const avatar_url = JSON.parse(asd)["avatar_url"]
-		const name = JSON.parse(asd)["name"]
-		const bio = JSON.parse(asd)["bio"]
 
-		const github = JSON.parse(asd)["html_url"]
-		const twitter = JSON.parse(asd)["twitter_username"]
-		const page = JSON.parse(asd)["blog"]
 
-		const persona = {
-			"nick": name,
-			"bio": bio,
-			"avatar": avatar_url,
-			"Twitter": twitter,
-			"GitHub": github,
-			"Page": page
-		};
-		list[i] = persona
+
+class Navbar extends Component<IProps, IState> {
+
+	constructor(props: any) {
+		super(props)
+		this.state = {
+
+		}
 
 	}
 
-	return await list;
-}
- const list = CardsList();
+	render() {
+		const { classes } = this.props;
+		return (
+			<React.Fragment>
+				<AppBar color="primary"  >
+					<Toolbar>
+						<Typography variant="h5" className={classes.Title} style={{ wordWrap: "break-word" }}>CD4ny!</Typography>
+						<IconButton color="inherit" aria-label="menu" onClick={() => {
 
-const Navbar = () => {
+						}}>
+							<MenuIcon>
+							</MenuIcon>
+						</IconButton>
+					</Toolbar>
+				</AppBar>
+				<div className={classes.offset}></div>
+				<Grid container spacing={0} alignContent="flex-start" justifyContent="flex-start">
+					
+					
+					
+					<PersonalCard nick="Daniel"></PersonalCard>
 
-	const classes = useStyle();
 
-	return (
-		<React.Fragment>
-			<AppBar color="primary"  >
-				<Toolbar>
-					<Typography variant="h5" className={classes.Title} style={{ wordWrap: "break-word" }}>CD4ny!</Typography>
-					<IconButton color="inherit" className={classes.menuIcon} aria-label="menu" onClick={() => {
-
-					}}>
-						<MenuIcon>
-						</MenuIcon>
-					</IconButton>
-				</Toolbar>
-			</AppBar>
-			<div className={classes.offset}></div>
-			<Grid container spacing={0} alignContent="flex-start" justifyContent="flex-start">
-				{list.map(
-					(d:any, i: Number) => {
-						return <PersonalCard nick={d.nick} bio={d.bio}></PersonalCard>
-					}
-				)
-				}
-			</Grid>
-		</React.Fragment>
-	);
+				</Grid>
+			</React.Fragment>
+		);
+	}
 };
 
-export default Navbar;
+export default withStyles(useStyle)(Navbar);
